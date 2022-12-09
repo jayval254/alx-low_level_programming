f947b3e628164ce517fecfffdda223aa67d90e3c
jayval254 <valariekimuge1@gmail.com>
2022-12-09 11:57:19 -0800

    variables_if_else_while

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - print letters in lowercase then in uppercase
+ * Return 0: on success
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'A' ; ch <= 'Z' ; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
