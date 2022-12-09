2534e0876c98a999fc2829909062567f77679328
jayval254 <valariekimuge1@gmail.com>
2022-12-09 13:13:10 -0800

    variables_if_else_while

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 /*
  * main - prints all possible combinations of single digit numbers
- * Description: using function main
- * this program prints "0, 1, 2, 3, 4, 5, 6, 7, 8, 9"
+ * Description: using main function
  * Return: 0 on success
  */
 int main(void)
