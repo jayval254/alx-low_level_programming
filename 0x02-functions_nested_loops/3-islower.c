4deb1f620a817d5f8f17c0490566bc2303ecaa4f
jayval254 <valariekimuge1@gmail.com>
2022-12-13 12:42:38 -0800

    variables_if_else_while

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _islower - check if the character is lowercase
+ * Return: Return 1 if c is lowercase and return 0 if otherwise
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
