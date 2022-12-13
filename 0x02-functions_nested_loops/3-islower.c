9982d48494882459efc37f505739f2c173e8daeb
jayval254 <valariekimuge1@gmail.com>
2022-12-13 12:45:16 -0800

    variables_if_else_while

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
  * _islower - check if the character is lowercase
+ *  @c: a charater argument
  * Return: Return 1 if c is lowercase and return 0 if otherwise
  */
 int _islower(int c)
