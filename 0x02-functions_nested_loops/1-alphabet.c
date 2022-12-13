746df2529de4e555ab51dfb65a5f1d640598352e
jayval254 <valariekimuge1@gmail.com>
2022-12-13 10:47:09 -0800

    variables_if_else_while

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,7 +2,7 @@
 /**
  * main - print the alphabet in lowercase
  * Return: always return 0 on success
- * Description: prints the lowercase alphabets, followed by a new line
+ * print_alphabet: prints the lowercase alphabets, followed by a new line
 */
 void print_alphabet(void)
 {
