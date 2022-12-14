b8ac050341c8c3e5e6863eb3af94a71d0c01cba0
jayval254 <valariekimuge1@gmail.com>
2022-12-14 04:31:55 -0800

    functions_nested_loops

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,8 +7,26 @@
  */
 void print_to_98(int n)
 {
-	for (n=1; n <= 98; n++)
+	if (n <= 98)
 	{
-		printf("%d \n", n);
+		for (; n <= 98; n++)
+		{
+			printf("%d", n);
+			if (n == 98)
+				continue;
+			printf(", ");
+		}
+		printf("\n");
+	}
+	else
+	{
+		for (; n >= 98; n--)
+		{
+			printf("%d", n);
+			if (n == 98)
+				continue;
+			printf(", ");
+		}
+		printf("\n");
 	}
 }
