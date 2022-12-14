76949c51ad9df4a6bc29d906e48cc9f804509159
jayval254 <valariekimuge1@gmail.com>
2022-12-14 00:29:39 -0800

    variables_if_else_while

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _abs - return absolute value of an integer
+ * @n - parameter
+ * Return: always 0 on success
+ */
+int _abs(int a)
+{
+if (a < 0)
+	a = -a;
+		return (a);
+}	
