04067cc6d2e0ad01e6e6596f60da436612928887
jayval254 <valariekimuge1@gmail.com>
2022-12-15 13:26:16 -0800

    more_funtions_more_nested_loops

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,18 +10,23 @@
 int main(void)
 {
 	int i;
+	char f[] = "Fizz";
+	char b[] = "Buzz";
+	char fb[] = "FizzBuzz";
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 15 == 0)
-			printf("FizzBuzz ");
+		if (i == 100)
+			printf("%s", b);
+		else if ((i % 3 == 0) && (i % 5 == 0))
+			printf("%s ", fb);
 		else if (i % 3 == 0)
-			printf("Fizz ");
+			printf("%s ", f);
 		else if (i % 5 == 0)
-			printf("Buzz ");
+			printf("%s ", b);
 		else
 			printf("%d ", i);
 	}
-	printf("Buzz\n");
+	printf("\n");
 	return (0);
 }
