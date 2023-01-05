b6f891bc34f77b3c7e33fc2344f69dccf9279eee
jayval254 <valariekimuge1@gmail.com>
2023-01-05 04:18:26 -0800

    C - Recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -6,9 +6,8 @@
  */
 void _print_rev_recursion(char *s)
 {
-	if (s[0] != '\0')
-	{
-		_putchar(s[0]);
-		_print_rev_recursion(s + 1);
-	}
+	if (*s == '\0')
+		return;
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
 }
