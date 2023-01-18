fd41b281c1384dcfe0b5375ce61063cf6218ea93
jayval254 <valariekimuge1@gmail.com>
2023-01-18 10:05:31 -0800

    Structures, typedef

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * new_dog - function that creates a new dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ * Return: NULL if the function fails
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+
---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog - function that frees dogs
+ * @d: pointer to dogs
+ * Return: 0 on success
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+	free(d->name);
+	free(d->age);
+	free((*d).owner);
+	free(d);
+}
